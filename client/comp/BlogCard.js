762433163d57d4501274fdbbb569d2043d688561
varshilj.patel <mail.varshil@gmail.com>
2023-04-19 23:54:58 +0530

    v0.6

--- client/comp/BlogCard.js
+++ client/comp/BlogCard.js
@@ -4,9 +4,10 @@ import { BsArrowRight } from 'react-icons/Bs'
 const BlogCard = () => {
   return (
     <>
-      <div className='flex shadow-lg p-5 gap-x-5 bg-white max-w-[800px] w-[90%] h-[200px] my-4'>
-        <div className='overflow-hidden items-center justify-center w-[33.3333%] h-full bg-slate-800'></div>
-        <div className='overflow-hidden relative w-[66.6666%] h-full'>
+      <div className='flex items-center md:flex-row flex-col shadow-lg p-5 gap-5 bg-white max-w-[900px] w-[95%] h-[500px] md:h-[200px] my-4'>
+        {/* image container */}
+        <div className='overflow-hidden items-center justify-center w-[95%] md:w-[33.3333%] h-[45%] md:h-full bg-slate-800'></div>
+        <div className='overflow-hidden relative w-[95%] md:w-[66.6666%] h-[55%] md:h-full'>
           <h3 className='text-lg9 font-bold text-black'>
             Lets know something about coding gerney about a life of programmer in their life
           </h3>
@@ -16,7 +17,7 @@ event - compiled client and server successfully in 170 ms (196 modules)
 event - compiled client and server successfully in 144 ms (196 modules)
 wait  - compiling...
           </p>
-          <button type='button' className=' flex items-center gap-x-2 rounded-sm bg-slate-900 text-white text-xs font-semibold py-[5px] px-4 absolute bottom-0 left-0'>
+          <button type='button' className=' flex items-center gap-x-2 rounded-sm bg-slate-900 text-white text-xs font-semibold py-1.5 px-4 absolute bottom-0 left-0'>
             Read More <BsArrowRight className='text-sm'></BsArrowRight>
           </button>
         </div>
--- client/pages/index.js
+++ client/pages/index.js
@@ -8,10 +8,9 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className='flex justify-center flex-col items-center'>
-      <CSlider />
-      <BlogCard />
-      <BlogCard />
-      <BlogCard />
+      <header className='w-full h-[50vh] overflow-hidden mb-10'>
+        <img className='h-full md:-translate-y-[50vh] md:h-auto md:w-full' src='asset-1.jpg'></img>
+      </header>
       <BlogCard />
       <BlogCard />
     </main>
